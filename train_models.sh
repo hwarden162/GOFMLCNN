c69c7c47f254e8f6d4da9cd0e435d12ddd9d267c
Hugh Warden <hugh.warden@outlook.com>
2025-11-18 13:45:30 +0000

    Add submission code

---
+++ train_models.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -N TrainModels
+#$ -pe sharedmem 8
+#$ -cwd
+#$ -l h_vmem=32G
+#$ -l h_rt=36:00:00
+#$ -o ./submission_logs/
+#$ -e ./submission_logs/
+
+. /etc/profile.d/modules.sh
+
+module load anaconda/2024.02
+
+source activate /exports/igmm/eddie/khamseh-lab/hwarden/GOFMLCNN/env
+
+python ./03_train_resnets.py
